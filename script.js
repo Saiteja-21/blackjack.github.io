0b5c9d381d3906069c413c62a7f8d0e47ce55d85
Saiteja-21 <saitejakatkam123@gmail.com>
2023-01-23 12:45:54 -0500

    step 4 added

--- script.js
+++ script.js
@@ -53,7 +53,8 @@ class Deck {
      * Shuffling the cards
      */
     shuffle() {
-        for(let j=this.deck.length,i=0;i<j;i++){
+        let j=this.deck.length;
+        for(let i=0;i<j;i++){
             let suits=Math.floor(Math.random()*j)
             let values=this.deck[i];
             this.deck[i]=this.deck[suits];
@@ -174,16 +175,36 @@ function initialDeal() {
     // Deal(Instantiate) 2 Dealer cards and 2 Player cards
 
     // write your code here
+    card1=new Card(deck.deal());
+    card2=new Card(deck.deal());
+    playerCard1=new Card(deck.deal());
+    playerCard2=new Card(deck.deal());
+
 
 
     // Open the board with 2 Dealer cards (one Dealer card is closed) and 2 Player cards (both open)
 
     // write your code here
+    card1.displayCard('card1',1);
+    card2.displayCard('card2',0);
+    playerCard1.displayCard('playerCard1',1);
+    playerCard2.displayCard('playerCard2',1);
+
+    
+
 
 
     // Setting face card values to 10
 
     // write your code here
+    card1.value=card1.value>10?10:card1.value;
+    card2.value=card2.value>10?10:card2.value;
+    playerCard1.value=playerCard1.value>10?10:playerCard1.value;
+    playerCard2.value=playerCard2.value>10?10:playerCard2.value;
+
+
+
+
 
 
     // Getting player cards total - show an alert only if there is a Blackjack
@@ -201,6 +222,19 @@ function initialDeal() {
     */
 
     // write your code here
+    playerTotal=playerCard1.value+playerCard2.value;
+    if(playerTotal==21){
+        cuteAlert({
+            type:'success',
+            title: "superb!!!",
+            message: "Blackjacked !!!",
+            buttonText: "wohoo !!!",
+            img:"success.svg"
+        }).then(() => {
+            location.reload()
+        })
+    }
+
 
 } //End of deal()
 
@@ -216,6 +250,31 @@ function stand() {
     // Checking Dealer and Player score - to give the result using cuteAlerts (just like the alert in initialDeal function)
 
     // write your code here
+    card2.flip();
+    dealerTotal=card1.value+card2.value;
+    if(playerTotal>=dealerTotal){
+    cuteAlert({
+        type:'success',
+        title:"congratulations !!!",
+        message:"You won the game",
+        buttonText:"yayy !",
+        img:"success.svg"
+        
+    }).then(() => {
+        location.reload()
+    })
+}
+    else{
+        cuteAlert({
+        type:"error",
+        title:"oh no !!!",
+        message:"dealer won the game",
+        buttonText:"ok",
+        img:"error.svg"
+    }).then(() => {
+        location.reload()
+    })}
+    
 
 }
 
@@ -231,6 +290,29 @@ function hit() {
     // Dealing the extra cards that the player requests
 
     // write your code here
+    playerCard3=new Card(deck.deal());
+    playerCard4=new Card(deck.deal());
+    if(extraCnt ==0){
+        playerCard3.displayCard('playerCard3',1);
+        playerCard3.value=playerCard3>10?10:playerCard3.value;
+        playerTotal=playerTotal+playerCard3.value;
+    }
+    else if(extraCnt ==1){
+        playerCard4.displayCard('playerCard4',1);
+        playerCard4.value=playerCard4>10?10:playerCard4.value;
+        playerTotal=playerTotal+playerCard4.value;
+
+    }
+    else{
+        dealButton.style.display='none';
+        cuteAlert({
+            type:'warning',
+            title:'sorry',
+            message:'max cards dealed',
+            buttonText:'ok',
+            img:'warning.svg'
+        })
+    }
 
 
     // Dealing new cards 
@@ -249,6 +331,26 @@ function hit() {
     */
 
     // write your code here
+    if(playerTotal>21){
+        cuteAlert({
+            type:'error',
+            title:'busted',
+            message:'you lost the game',
+            buttonText:'ok',
+            img:'error.svg'
+        }).then(() =>{location.reload()})
+    }
+    else{
+        21==playerTotal&&cuteAlert({
+            type:'success',
+            title:'superb',
+            message:'blackjacked',
+            buttonText:'wooh',
+            img:'success.svg'
+        }).then(() => {
+            location.reload()
+        });
+    }
 
 
     // Checking the total of the player cards before dealing new cards
